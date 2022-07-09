16ab74e404bdf8c9db9ac83b08168f64cfe243dc
Akash <akashkumarojha98@gmail.com>
2022-07-09 16:09:33 +0000

    adding copy yaml file

--- main.tf
+++ main.tf
@@ -68,9 +68,9 @@ resource "aws_instance" "linux" {
 
 
 resource "aws_instance" "ubuntu" {
-  ami           = "ami-04505e74c0741db8d"
-  instance_type = "t2.micro"
-  key_name      = aws_key_pair.deployer1.key_name
+  ami                    = "ami-04505e74c0741db8d"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.deployer1.key_name
   vpc_security_group_ids = ["${aws_security_group.allow_SSH.id}"]
   tags = {
     "Name" = "UBUNTU-Node"
